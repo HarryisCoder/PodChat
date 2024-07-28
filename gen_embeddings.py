f549adc7b2c68770b7c623ebfde4332f6bd06e09
Harry Cheng <tianshuc@Harrys-MacBook-Pro.local>
2024-07-28 12:00:38 -0700

    update embedding to using openai 1.0+

--- gen_embeddings.py
+++ gen_embeddings.py
@@ -1,8 +1,11 @@
-import openai  # for generating embeddings
+from openai import OpenAI  # for generating embeddings
 import os
 import pandas as pd  # for DataFrames to store article sections and embeddings
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 def read_file_with_custom_line_breaker(file_path, line_breaker):
     try:
@@ -21,10 +24,10 @@ def calculate_embeddings(csv_file_path, strings, embedding_model, batch_size):
         batch_end = batch_start + batch_size
         batch = strings[batch_start:batch_end]
         print(f"Batch {batch_start} to {batch_end}")
-        response = openai.Embedding.create(model=embedding_model, input=batch)
-        for i, be in enumerate(response["data"]):
-            assert i == be["index"]  # double check embeddings are in same order as input
-        batch_embeddings = [e["embedding"] for e in response["data"]]
+        response = client.embeddings.create(model=embedding_model, input=batch)
+        for i, be in enumerate(response.data):
+            assert i == be.index  # double check embeddings are in same order as input
+        batch_embeddings = [e.embedding for e in response.data]
         embeddings.extend(batch_embeddings)
     df = pd.DataFrame({"text": strings, "embedding": embeddings})
     df.to_csv(SAVE_PATH, index=False)
@@ -43,10 +46,10 @@ def calculate_new_embeddings_only(csv_file_path, strings, embedding_model, batch
         batch_end = batch_start + batch_size
         batch = strings[batch_start:batch_end]
         print(f"Batch {batch_start} to {len(batch)-1}")
-        response = openai.Embedding.create(model=embedding_model, input=batch)
-        for i, be in enumerate(response["data"]):
-            assert i == be["index"]  # double check embeddings are in same order as input
-        batch_embeddings = [e["embedding"] for e in response["data"]]
+        response = client.embeddings.create(model=embedding_model, input=batch)
+        for i, be in enumerate(response.data):
+            assert i == be.index  # double check embeddings are in same order as input
+        batch_embeddings = [e.embedding for e in response.data]
         embeddings.extend(batch_embeddings)
     new_df = pd.DataFrame({"text": strings[new_start_idx:], "embedding": embeddings})
     new_df_rows = new_df.shape[0]
@@ -68,9 +71,8 @@ string_list = read_file_with_custom_line_breaker(file_path, line_breaker)
 #     print(f"string {i}: {string_list[i]}\n")
 
 # calculate embeddings
-EMBEDDING_MODEL = "text-embedding-ada-002"  # OpenAI's best embeddings as of Apr 2023
-BATCH_SIZE = 1000  # you can submit up to 2048 embedding inputs per request
+EMBEDDING_MODEL = "text-embedding-3-large"  # OpenAI's best embeddings as of Apr 2023
+BATCH_SIZE = 1  # you can submit up to 2048 embedding inputs per request
 SAVE_PATH = "resources/show_notes_embeddings.csv"
-# calculate_embeddings(SAVE_PATH, string_list, EMBEDDING_MODEL, BATCH_SIZE)
-calculate_new_embeddings_only(SAVE_PATH, string_list, EMBEDDING_MODEL, BATCH_SIZE)
-# df.to_csv(SAVE_PATH, index=False)
+calculate_embeddings(SAVE_PATH, string_list, EMBEDDING_MODEL, BATCH_SIZE)
+# calculate_new_embeddings_only(SAVE_PATH, string_list, EMBEDDING_MODEL, BATCH_SIZE)
